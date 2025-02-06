94e6ebf51bef432f876fb15d25572a783731d389
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-02-06 18:14:24 +0600

    update portfolio

--- src/Components/home/Skills/SkillsEducation.jsx
+++ src/Components/home/Skills/SkillsEducation.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-import { FaCss3Alt, FaHtml5, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
+import {
+  FaCcStripe,
+  FaCss3Alt,
+  FaHtml5,
+  FaJs,
+  FaLinode,
+  FaNodeJs,
+  FaReact,
+  FaStarOfLife,
+} from "react-icons/fa";
+import { FaGalacticRepublic } from "react-icons/fa6";
+import { GiPayMoney } from "react-icons/gi";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 import { RiTailwindCssFill } from "react-icons/ri";
-import { SiExpress, SiReactrouter } from "react-icons/si";
+import { SiReacthookform } from "react-icons/si";
+
+import {
+  SiExpress,
+  SiMaterialdesign,
+  SiMongodb,
+  SiReactrouter,
+} from "react-icons/si";
 
 const SkillsEducation = () => {
   return (
@@ -29,37 +48,78 @@ const SkillsEducation = () => {
               </div>
               <ul className="flex flex-wrap gap-16 text-center justify-center items-center">
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <FaHtml5 className="text-4xl text-orange-500" />
+                  <FaHtml5 className="text-4xl text-[#f97316]" />
                   <h4 className="text-xl font-semibold">HTML</h4>
                 </li>
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <FaCss3Alt className="text-4xl text-blue-600" />
+                  <FaCss3Alt className="text-4xl text-[#2563eb]" />
                   <h4 className="text-xl font-semibold">Css</h4>
                 </li>
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <RiTailwindCssFill className="text-4xl text-sky-400" />
+                  <RiTailwindCssFill className="text-4xl text-[#38bdf8]" />
                   <h4 className="text-xl font-semibold">Tailwind Css</h4>
                 </li>
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <FaJs className="text-4xl text-yellow-400" />
+                  <SiMaterialdesign className="text-4xl text-[#3476e0]" />
+                  <h4 className="text-xl font-semibold">Material Css UI</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <FaJs className="text-4xl text-[#facc15]" />
                   <h4 className="text-xl font-semibold">Javascript</h4>
                 </li>
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <FaReact className="text-4xl text-cyan-400" />
+                  <FaReact className="text-4xl text-[#22d3ee]" />
                   <h4 className="text-xl font-semibold">React js</h4>
                 </li>
-
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <SiReactrouter className="text-4xl text-orange-600" />
+                  <SiReactrouter className="text-4xl text-[#ca8a04]" />
                   <h4 className="text-xl font-semibold">React Rouder Dom</h4>
                 </li>
                 <li className=" flex flex-col text-center justify-center items-center">
-                  <FaNodeJs className="text-4xl text-lime-500" />
+                  <FaGalacticRepublic className="text-4xl text-[#f8a932]" />
+                  <h4 className="text-xl font-semibold">TanStack Query</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <SiReacthookform className="text-4xl text-[#f8a932]" />
+                  <h4 className="text-xl font-semibold"> React Hook Form</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <FaNodeJs className="text-4xl text-[#84cc16]" />
                   <h4 className="text-xl font-semibold">Node js</h4>
                 </li>
                 <li className=" flex flex-col text-center justify-center items-center">
+                  <SiMongodb className="text-4xl text-[#84cc16]" />
+                  <h4 className="text-xl font-semibold">MongoDb</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <FaStarOfLife className="text-4xl text-[#9333ea]" />
+                  <h4 className="text-xl font-semibold">Json Web Token</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <FaLinode className="text-4xl text-[#22d3ee]" />
+                  <h4 className="text-xl font-semibold">NodeMailer</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <MdOutlineAlternateEmail className="text-4xl text-[#f3723f]" />
+                  <h4 className="text-xl font-semibold">Mailgun</h4>
+                </li>
+
+                <li className=" flex flex-col text-center justify-center items-center">
                   <SiExpress className="text-4xl" />
                   <h4 className="text-xl font-semibold">Express js</h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <FaCcStripe className="text-4xl text-[#1fa84d]" />
+                  <h4 className="text-xl font-semibold">
+                    Stripe payment method
+                  </h4>
+                </li>
+                <li className=" flex flex-col text-center justify-center items-center">
+                  <GiPayMoney className="text-4xl text-[#1f529a]" />
+                  <h4 className="text-xl font-semibold">
+                    {" "}
+                    sslcommerz payment method
+                  </h4>
                 </li>
               </ul>
             </div>
